5d21cb767943e9a67a13b08cb018faaf35edbb27
Chad Neff <ccneff@gmail.com>
2016-02-29 16:33:55 -0800

    added input elements to create an interactive list

--- src/components/todo/todolist.js
+++ src/components/todo/todolist.js
@@ -3,26 +3,45 @@ import { connect } from 'react-redux';
 
 export const Todo = React.createClass({
   getInitialState() {
+    
     return {
-      items: [
-        { text: 'Some TODO item', isChecked: false },
-        { text: 'Some TODO item', isChecked: false },
-        { text: 'Some TODO item', isChecked: true },
-        { text: 'Some TODO item', isChecked: false }
-      ]
+      items: []
     };
+  },
+  additem(ev){
+    if(ev.keyCode == 13){
+      this.state.items.push({id : this.state.items.length, 
+                            text : this.aToDo.value,
+                            isComplete : false});
+      this.setState(this.state.items);
+      this.aToDo.value = "";
+    }
+    //console.log(ev.keyCode);
+    // this.items = {}
+  },
+  checked(ev){
+    if(this.completedCheck.checked){
+      
+      this.setState(this.state.items);
+    }
   },
   render() {
     return (
       <div>
-        <pre>{JSON.stringify(this.state, null, 2)}</pre>
+         <input type="text" ref={(ref) => this.aToDo = ref} onKeyUp={this.additem}/>
         {this.state.items.map(i => {
-          if(i.isChecked){
-            return (<li className = "disabled">{i.text}</li>)
+          if(i.isComplete){
+            return (<div className = "disabled">{i.text}</div>)
           } else {
-            return (<li>{i.text}</li>);
+            return (
+              <div>
+                <input type="checkbox" ref={(ref) => this.completedCheck = ref} onClick={this.checked}/>
+                {i.text}
+              </div>
+             );
           }
         })}
+        <pre>{JSON.stringify(this.state, null, 2)}</pre>
       </div>
     );
   }
