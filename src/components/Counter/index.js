4dda8e639d6b41b8d566747971433c5da5410641
Gabe Isenberg <gisenberg@godaddy.com>
2016-03-07 13:57:32 -0800

    First Commit

--- src/components/Counter/index.js
+++ src/components/Counter/index.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-const Counter = React.createClass({
+/// <HelloWorld text="foo" />
+
+export const Counter = React.createClass({
   propTypes: {
     value: React.PropTypes.number.isRequired,
     onIncrementClicked: React.PropTypes.func.isRequired,
@@ -9,7 +11,7 @@ const Counter = React.createClass({
   },
   render() {
     return (
-      <div>
+      <div> 
         <h1>Counter</h1>
         <p>{this.props.value}</p>
         <button onClick={this.props.onIncrementClicked}>+</button>
@@ -19,9 +21,52 @@ const Counter = React.createClass({
   }
 });
 
+export const StateCounter = React.createClass({
+  getInitialState() {
+    return {
+      value: 5,
+    };
+  },
+  increment(){
+    const  oldVal = this.state.value;
+    this.setState({
+      value: oldVal +1
+    })
+  },
+  decrement(){
+      const  oldVal = this.state.value;
+    this.setState({
+      value: oldVal -1
+    })
+  },
+  render(){
+    return (
+      <Counter 
+        value={this.state.value}
+        onIncrementClicked={this.increment} 
+        onDecrementClicked={this.decrement} />
+      );
+  }
+})
+
+export const TodoItem = React.createClass({
+  propTypes: {
+    name: React.PropTypes.string.isRequired,
+    isDone: React.PropTypes.bool.isRequired
+  },
+  render(){
+    return (
+      <li>
+      <span>{this.props.name}</span>
+      <input type="checkbox" checked={this.props.isDone}/>
+      </li>
+      )
+  }
+})
+
 export default connect(
-  (state) => { return { value: state } },
-  (dispatch) => {
+  (state) => { return { value: state } }, // mapStateToProps
+  (dispatch) => { 
     return {
       onIncrementClicked: () => dispatch({ type: 'INCREMENT' }),
       onDecrementClicked: () => dispatch({ type: 'DECREMENT' })
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Counter from './components/Counter';
+import ReduxCounter, { Counter, StateCounter, TodoItem } from './components/Counter';
 import reducer from './reducers/counter';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
@@ -8,5 +8,12 @@ import { createStore } from 'redux';
 const store = createStore(reducer);
 ReactDOM.render((
   <Provider store={store}>
-    <Counter />
+	<div>
+	    <ReduxCounter />
+	    <StateCounter />
+	    <h2>MY LIST</h2>
+	    <ul>
+	    <TodoItem  name="hello" isDone={true} />
+	    </ul>
+    </div>
   </Provider>), document.getElementById('root'));
