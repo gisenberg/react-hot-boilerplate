4c0d046d8b318dcc180fc8470e1d8b0ee9311102
Chad Neff <ccneff@gmail.com>
2016-08-10 12:21:13 -0700

    Revert "Started todolist"
    This reverts commit 343f9e6098dd06d3c5a3db3e8856513668014424.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Counter from './components/Counter';
-import TodoList from './components/TodoList';
 import reducer from './reducers/counter';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
 const store = createStore(reducer);
-// ReactDOM.render((
-//   <Provider store={store}>
-//     <Counter />
-//   </Provider>), document.getElementById('root'));
-
 ReactDOM.render((
-    <TodoList />
- ), document.getElementById('root'));
+  <Provider store={store}>
+    <Counter />
+  </Provider>), document.getElementById('root'));
